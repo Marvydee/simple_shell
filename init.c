62898fd3b3628ec2c0340c164b1445f771c7838b
Marvydee <marvydee1@gmail.com>
2023-12-05 10:17:11 +0100

    added command and tokenize files

---
+++ init.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * tokenize_input - Tokenize the input string into arguments.
+ * Uses strtok to split the string based on spaces.
+ * @buffer: The input string to be tokenized.
+ * @args: An array to store the resulting tokens.
+ */
+void tokenize_input(char *buffer, char **args)
+{
+	char *token = strtok(buffer, " ");
+	int i = 0;
+
+	while (token != NULL)
+	{
+		args[i++] = token;
+		token = strtok(NULL, " ");
+	}
+
+	args[i] = NULL; /*Null-terminate the array*/
+}
+
