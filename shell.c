49588e778977c29abb3ff7463ccc5b52b53d2d81
Frankiln <odinakachukwufranklin0@gmail.com>
2023-12-10 17:05:24 -0800

    update shell main

--- shell.c
+++ shell.c
@@ -1,14 +1,15 @@
 #include "shell.h"
+
 /**
- * main - entry point
- * Return: always 0
+ * main - Entry point for the shell program.
+ * Return: Always 0.
  */
 int main(void)
 {
 	char buffer[BUFFER_SIZE];
 
-	/*Set up the PATH environment variable*/
-	char *path = "/bin:/usr/bin"; /*Update with your desired PATH*/
+	/* Set up the PATH environment variable */
+	char *path = "/bin:/usr/bin";	/* Update with your desired PATH */
 
 	setenv("PATH", path, 1);
 
@@ -18,14 +19,29 @@ int main(void)
 
 		if (fgets(buffer, sizeof(buffer), stdin) == NULL)
 		{
-			write(STDOUT_FILENO, "\n", 1); /*Handle Ctrl+D (EOF)*/
+			write(STDOUT_FILENO, "\n", 1);	/* Handle Ctrl+D (EOF) */
 			break;
 		}
 
-		/*Remove the newline character*/
+		/* Remove the newline character */
 		buffer[strcspn(buffer, "\n")] = '\0';
 
-		execute_command(buffer);
+		if (strcmp(buffer, "env") == 0)
+		{
+			/* Handle the env built-in */
+			char **env = environ;
+
+			while (*env != NULL)
+			{
+				write(STDOUT_FILENO, *env, strlen(*env));
+				write(STDOUT_FILENO, "\n", 1);
+				env++;
+			}
+		}
+		else
+		{
+			execute_command(buffer);
+		}
 	}
 
 	return (0);
